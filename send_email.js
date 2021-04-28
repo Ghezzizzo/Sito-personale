4bd0e0ef34f5d1340d149a870728caf122103fee
Guido Ghezzi <ghezzizzo@gmail.com>
2021-04-28 20:57:54 +0200

    form finalmente funzionante

---
+++ send_email.js
@@ -0,0 +1,18 @@
+(function() {
+emailjs.init("user_8gJqvQsN5pKwEZCZpGg3j");
+})();
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('service_blt3n1h', 'template_ertv74l', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
