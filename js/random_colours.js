b9eb8f46d9b1472bab33c0febf15b4717808f507
Guido Ghezzi <ghezzizzo@gmail.com>
2021-04-30 00:16:34 +0200

    suddiviso file, ho suddiviso il file random_colours.html

---
+++ js/random_colours.js
@@ -0,0 +1,53 @@
+const col1 = document.querySelector('.col-1');
+const col2 = document.querySelector('.col-2');
+const col3 = document.querySelector('.col-3');
+const col4 = document.querySelector('.col-4');
+
+const btn = document.querySelector('button');
+
+let interval
+let time = 0;
+
+let colours = ['red','green','blue','yellow'];
+
+btn.addEventListener('click',startTimer);
+
+function startTimer() {
+    if (btn.textContent === 'Start') {
+        interval = setInterval(timer, 1000);
+        btn.textContent = 'Stop';
+    } else {
+        clearInterval(interval)
+        btn.textContent = 'Start';
+        col1.style.backgroundColor = 'white';
+        col2.style.backgroundColor = 'white';
+        col3.style.backgroundColor = 'white';
+        col4.style.backgroundColor = 'white';
+        time = 0
+        document.getElementById('time').innerHTML = 0;
+    }
+
+}
+
+function getRandomInt (min,max) {
+    mini = Math.ceil(min);
+    maxi = Math.floor(max);
+    return Math.floor(Math.random()*(maxi-mini)) + mini;
+}
+
+function timer () {
+        
+    if (time > 1) {
+        time--;
+    } else {
+        time = 5;
+        document.getElementById('time').innerHTML = time;
+        col1.style.backgroundColor = colours[getRandomInt (0,4)];
+        col2.style.backgroundColor = colours[getRandomInt (0,4)];
+        col3.style.backgroundColor = colours[getRandomInt (0,4)];
+        col4.style.backgroundColor = colours[getRandomInt (0,4)];  
+    }
+    document.getElementById('time').innerHTML = time;
+        
+}
+
